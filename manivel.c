acf74f21fb95c8591db59d208c4a66039906ef8f
SUBRAMANIVEL <30345172+SUBRAMANIVEL@users.noreply.github.com>
2017-08-29 16:21:58 +0530

    Create manivel.c

---
+++ manivel.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+void main()
+{
+int x,y,z;      
+scanf("%d",&x);
+scanf("%d",&y);
+scanf("%d",&z);
+if(x>y||x>z)
+{
+{
+printf("x is great");
+}
+if else(y>x||y>z)
+{
+printf("y is great");
+}
+else
+{
+printf("z is great");
+}
+}
+}
